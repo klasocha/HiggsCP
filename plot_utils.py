96f88ebee29a2f7857d2c2438a7d67a938796923
klasocha <kacper.lasocha@gmail.com>
2019-05-05 15:26:44 +0200

    add error plot

--- plot_utils.py
+++ plot_utils.py
@@ -30,6 +30,24 @@ def feature_plot(data, directory, filename, w_a, w_b , filt,step=0.05):
 	plt.show()
     plt.clf()
 
+def error_plot(data, directory, filename, filt, step=0.05):
+	
+	bins = int(1/step) + 1
+	plt.hist(data, bins)
+	
+	ax = plt.gca()
+        ax.annotate("Relative RMS = {:0.3f} \nRelative mean = {:1.3f}".format(np.sqrt(np.mean((data)**2)), np.mean(data)), xy=(0.65, 0.85), xycoords='axes fraction', fontsize=12)
+
+	plt.tight_layout()
+
+	if filename:
+		plt.savefig(directory + filename+".eps")
+		
+	else:
+		plt.show()
+	plt.clf()
+
+
 def monit_plots(args, event, w_a, w_b):
 
     if args.PLOT_FEATURES == "FILTER":
@@ -44,9 +62,7 @@ def monit_plots(args, event, w_a, w_b):
     for i in range(len(event.cols[0,:])-1):
         feature_plot(event.cols[:,i], directory = "../monit_plots/" + args.TYPE + "_" + args.FEAT + "_Unweighted_" + str(args.UNWEIGHTED) + "_" + args.PLOT_FEATURES + "/",
                          filename = event.labels[i], w_a = w_a, w_b = w_b, filt = filt)
-    for i in range(len(event.labels_suppl)):
-        feature_plot(event.cols_suppl[:,i], directory = "../monit_plots/" + args.TYPE + "_" + args.FEAT + "_Unweighted_" + str(args.UNWEIGHTED) + "_" + args.PLOT_FEATURES + "/",
-                         filename = event.labels_suppl[i], w_a = w_a, w_b = w_b, filt = filt)
+
         
     if args.FEAT == "Variant-1.1" and args.TYPE == "nn_rhorho": #acoangle depending on y1y2 sign
         y1y2_pos = np.array(event.cols[:,-3][filt]*event.cols[:,-2][filt] >= 0)
@@ -184,3 +200,11 @@ def monit_plots(args, event, w_a, w_b):
                          filename = "aco_angle_16_y1y2_pos", w_a = w_a*y1y2_pos, w_b = w_b*y1y2_pos, filt = filt)
         feature_plot(event.cols[:,-10], directory = "../monit_plots/" + args.TYPE + "_" + args.FEAT + "_Unweighted_" + str(args.UNWEIGHTED) + "_" + args.PLOT_FEATURES + "/",
                          filename = "aco_angle_16_y1y2_neg", w_a = w_a*y1y2_neg, w_b = w_b*y1y2_neg, filt = filt)
+
+    if args.FEAT == "Variant-4.1":
+	for i in range(8):
+	    error_plot(event.cols_suppl[:,i], directory = "../monit_plots/" + args.TYPE + "_" + args.FEAT + "_Unweighted_" + str(args.UNWEIGHTED) + "_" + args.PLOT_FEATURES + "/",
+		                 filename = event.labels_suppl[i], filt = filt)
+	for i in range(8,len(event.labels_suppl)):
+	    feature_plot(event.cols_suppl[:,i], directory = "../monit_plots/" + args.TYPE + "_" + args.FEAT + "_Unweighted_" + str(args.UNWEIGHTED) + "_" + args.PLOT_FEATURES + "/",
+		                 filename = event.labels_suppl[i], w_a = w_a, w_b = w_b, filt = filt)
