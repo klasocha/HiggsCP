8a9a1febaaad95b8a3ecf0d351c4b5052d9c369c
elarichterwas <Elzbieta.Richter-Was@uj.edu.pl>
2019-05-28 10:20:56 +0200

    working version of anal_class_num.py

--- anal_scripts/anal_class_num.py
+++ anal_scripts/anal_class_num.py
@@ -5,13 +5,31 @@ import os
 import matplotlib.pyplot as plt
 
 
-filenames = []
+filelist_rhorho_Variant_All = []
 
-filenames.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_2')
-filenames.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_4')
-filenames.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_8')
-filenames.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_10')
-filenames.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_16')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_2')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_4')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_6')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_8')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_10')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_12')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_14')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_16')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_18')
+filelist_rhorho_Variant_All.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_20')
+
+filelist_rhorho_Variant_4_1 = []
+
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_2')
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_4')
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_6')
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_8')
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_10')
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_12')
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_14')
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_16')
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_18')
+filelist_rhorho_Variant_4_1.append('npy/nn_rhorho_Variant-4.1_Unweighted_False_NO_NUM_CLASSES_20')
 
 def calculate_metrics(directory, num_class):
     calc_w = np.load(os.path.join(directory, 'softmax_calc_w.npy'))
@@ -35,16 +53,47 @@ def calculate_metrics(directory, num_class):
 
 
 
-metrics = [calculate_metrics(filenames[0], 2), calculate_metrics(filenames[1], 4),
-           calculate_metrics(filenames[2], 8), calculate_metrics(filenames[3], 10),
-           calculate_metrics(filenames[4], 16)]
+metrics_Variant_All = [calculate_metrics(filelist_rhorho_Variant_All[0], 2),  calculate_metrics(filelist_rhorho_Variant_All[1], 4),
+           calculate_metrics(filelist_rhorho_Variant_All[1], 6), calculate_metrics(filelist_rhorho_Variant_All[2], 8),
+           calculate_metrics(filelist_rhorho_Variant_All[3], 10), calculate_metrics(filelist_rhorho_Variant_All[3], 12),
+           calculate_metrics(filelist_rhorho_Variant_All[4], 14), calculate_metrics(filelist_rhorho_Variant_All[4], 16),
+           calculate_metrics(filelist_rhorho_Variant_All[4], 18), calculate_metrics(filelist_rhorho_Variant_All[4], 20)]
+           
+metrics_Variant_All = np.stack(metrics_Variant_All)
 
-metrics = np.stack(metrics)
 
-plt.plot(metrics[:, 0])
-plt.plot(metrics[:, 1])
-plt.plot(metrics[:, 2])
-plt.plot(metrics[:, 3])
+metrics_Variant_4_1 = [calculate_metrics(filelist_rhorho_Variant_4_1[0], 2),  calculate_metrics(filelist_rhorho_Variant_4_1[1], 4),
+           calculate_metrics(filelist_rhorho_Variant_4_1[1], 6), calculate_metrics(filelist_rhorho_Variant_4_1[2], 8),
+           calculate_metrics(filelist_rhorho_Variant_4_1[3], 10), calculate_metrics(filelist_rhorho_Variant_4_1[3], 12),
+           calculate_metrics(filelist_rhorho_Variant_4_1[4], 14), calculate_metrics(filelist_rhorho_Variant_4_1[4], 16),
+           calculate_metrics(filelist_rhorho_Variant_4_1[4], 18), calculate_metrics(filelist_rhorho_Variant_4_1[4], 20)]
+           
+metrics_Variant_4_1 = np.stack(metrics_Variant_4_1)
+
+
+# Now start plotting metrics
+# Make better plots here, add axes labels, add color labels, store into figures/*.eps files
+
+plt.plot(metrics_Variant_All[:, 0])
+plt.plot(metrics_Variant_All[:, 1])
+plt.plot(metrics_Variant_All[:, 2])
+plt.plot(metrics_Variant_All[:, 3])
 plt.show()
-plt.plot(metrics[:, 4])
+plt.plot(metrics_Variant_All[:, 4])
+plt.show()
+plt.plot(metrics_Variant_All[:, 5])
+plt.show()
+plt.plot(metrics_Variant_All[:, 6])
+plt.show()
+
+plt.plot(metrics_Variant_4_1[:, 0])
+plt.plot(metrics_Variant_4_1[:, 1])
+plt.plot(metrics_Variant_4_1[:, 2])
+plt.plot(metrics_Variant_4_1[:, 3])
+plt.show()
+plt.plot(metrics_Variant_4_1[:, 4])
+plt.show()
+plt.plot(metrics_Variant_4_1[:, 5])
+plt.show()
+plt.plot(metrics_Variant_4_1[:, 6])
 plt.show()
