241701ba6e1bf77ae6d99f119d5f8f336ff843c7
elarichterwas <Elzbieta.Richter-Was@uj.edu.pl>
2019-08-18 06:41:05 +0200

    problem in calculate_metrics of anal_utils.py

--- anal_py/anal_utils.py
+++ anal_py/anal_utils.py
@@ -36,24 +36,39 @@ def calculate_metrics(directory, num_classes):
     calc_arg_maxs = np.argmax(calc_w, axis=1)
     
     # ERW including here redefinition of  calc_pred_argmaxs_distances by Michal
+    # short cut is taken in the "sign", should not take mean of absolute value!!!
+    # example implementation from anal_py/plot_softmax_rhorho_Variant_All.py
+
+    # delt_argmax = np.argmax(calc_w[:], axis=1) - np.argmax(preds_w[:], axis=1)
+    # for i in range (len(delt_argmax)):
+    # if  delt_argmax[i] > num_classes/2.0 :
+    #     delt_argmax[i] = num_classes -  delt_argmax[i]
+    # if  delt_argmax[i] < - num_classes/2.0 :
+    #    delt_argmax[i] = - num_classes -  delt_argmax[i]
+    
     calc_pred_argmaxs_distances = np.min(
          np.stack(
             [np.abs(pred_arg_maxs-calc_arg_maxs), ((num_classes - 1) - np.abs(pred_arg_maxs-calc_arg_maxs))]
         ), axis=0)
 
-    # new definition from Michal
-    # something qrong with this definition, oscilating values
-    # calc_pred_argmaxs_distances = calc_argmaxs_distances(pred_arg_maxs, calc_arg_maxs, num_class)
-    
-    acc0 = (calc_pred_argmaxs_distances <= 0).mean()
-    acc1 = (calc_pred_argmaxs_distances <= 1).mean()
-    acc2 = (calc_pred_argmaxs_distances <= 2).mean()
-    acc3 = (calc_pred_argmaxs_distances <= 3).mean()
+    print calc_pred_argmaxs_distances
+    print np.mean(calc_pred_argmaxs_distances), np.mean(calc_pred_argmaxs_distances) * 360./(1.0*num_classes)
 
     mean_error = np.mean(calc_pred_argmaxs_distances)
     #ERW: scaled to radians and in units of alpha^CP
     k2PI = 6.28
-    mean_error_scaled = np.mean(calc_pred_argmaxs_distances/(1.0*num_classes) * k2PI/2. )
+    mean_error_scaled = np.mean(calc_pred_argmaxs_distances) * k2PI/(1.0*num_classes)
+    print mean_error_scaled
+    
+    # new definition from Michal
+    # something qrong with this definition, oscilating values
+    # calc_pred_argmaxs_distances = calc_argmaxs_distances(pred_arg_maxs, calc_arg_maxs, num_class)
+
+    # here it is OK, because we are interested in fractions
+    acc0 = (calc_pred_argmaxs_distances <= 0).mean()
+    acc1 = (calc_pred_argmaxs_distances <= 1).mean()
+    acc2 = (calc_pred_argmaxs_distances <= 2).mean()
+    acc3 = (calc_pred_argmaxs_distances <= 3).mean()
 
     # ERW
     # calc_w are not normalised to unity, while preds_w are
