Corrected calculation for mean distance, not yet finished, change in links to directory with .npy files

anal_py/anal_num_class_rhorho_Variant_All.py
@@ -4,27 +4,27 @@ import os, errno
 
 import matplotlib.pyplot as plt
 
-from anal_utils import calculate_metrics
+from anal_utils import calculate_metrics_from_file
 
 filelist_rhorho_Variant_1_1 = []
 
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_2')
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_4')
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_6')
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_8')
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_10')
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_12')
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_14')
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_16')
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_18')
-filelist_rhorho_Variant_1_1.append('npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_20')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_2/monit_npy/')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_4/monit_npy/')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_6/monit_npy/')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_8/monit_npy/')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_10/monit_npy/')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_12/monit_npy/')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_14/monit_npy/')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_16/monit_npy/')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_18/monit_npy/')
+filelist_rhorho_Variant_1_1.append('../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_20/monit_npy/')
 
 
-metrics_Variant_1_1 = [calculate_metrics(filelist_rhorho_Variant_1_1[0], 2),  calculate_metrics(filelist_rhorho_Variant_1_1[1], 4),
-                       calculate_metrics(filelist_rhorho_Variant_1_1[2], 6),  calculate_metrics(filelist_rhorho_Variant_1_1[3], 8),
-                       calculate_metrics(filelist_rhorho_Variant_1_1[4], 10), calculate_metrics(filelist_rhorho_Variant_1_1[5], 12),
-                       calculate_metrics(filelist_rhorho_Variant_1_1[6], 14), calculate_metrics(filelist_rhorho_Variant_1_1[7], 16),
-                       calculate_metrics(filelist_rhorho_Variant_1_1[8], 18), calculate_metrics(filelist_rhorho_Variant_1_1[9], 20)]
+metrics_Variant_1_1 = [calculate_metrics_from_file(filelist_rhorho_Variant_1_1[0], 2), calculate_metrics_from_file(filelist_rhorho_Variant_1_1[1], 4),
+                       calculate_metrics_from_file(filelist_rhorho_Variant_1_1[2], 6), calculate_metrics_from_file(filelist_rhorho_Variant_1_1[3], 8),
+                       calculate_metrics_from_file(filelist_rhorho_Variant_1_1[4], 10), calculate_metrics_from_file(filelist_rhorho_Variant_1_1[5], 12),
+                       calculate_metrics_from_file(filelist_rhorho_Variant_1_1[6], 14), calculate_metrics_from_file(filelist_rhorho_Variant_1_1[7], 16),
+                       calculate_metrics_from_file(filelist_rhorho_Variant_1_1[8], 18), calculate_metrics_from_file(filelist_rhorho_Variant_1_1[9], 20)]
            
 metrics_Variant_1_1 = np.stack(metrics_Variant_1_1)
 
@@ -75,7 +75,7 @@ filename = "rhorho_meanDelt_class_Variant-All_nc"
 
 plt.plot(x, metrics_Variant_1_1[:, 4],'o', label=r'$<\Delta>$ classes')
 
-plt.ylim([0.0, 3])
+#plt.ylim([0.0, 0.5])
 plt.xticks(x)
 plt.legend()
 plt.xlabel('Number of classes')
@@ -105,13 +105,13 @@ plt.clf()
 pathOUT = "figures/"
 filename = "rhorho_meanDelt_phiCPmix_Variant-All_nc"
 
-plt.plot(x, metrics_Variant_1_1[:, 7],'o', label=r'$<\Delta \alpha^{CP}>$ ')
+plt.plot(x, metrics_Variant_1_1[:, 7],'o', label=r'$<\Delta \phi^{CP}> [rad]$ ')
 
-plt.ylim([0.0, 0.5])
+#plt.ylim([0.0, 0.5])
 plt.xticks(x)
 plt.legend()
 plt.xlabel('Number of classes')
-plt.ylabel(r'$<\Delta \alpha^{CP}>$ (rad)')
+plt.ylabel(r'$<\Delta \phi^{CP}>$ (rad)')
 plt.title('Feautures list: Variant-All')
 
 ax = plt.gca()

anal_py/anal_utils.py
@@ -27,27 +27,31 @@ def calculate_metrics_from_file(directory, num_classes):
     preds_w = np.load(os.path.join(directory, 'softmax_preds_w.npy'))
     return calculate_metrics(calc_w, preds_w, num_classes)
 
-def calculate_metrics(directory, num_classes):
-    calc_w  = np.load(os.path.join(directory, 'softmax_calc_w.npy'))
-    preds_w = np.load(os.path.join(directory, 'softmax_preds_w.npy'))    
+#def calculate_metrics(directory, num_classes):
+#    calc_w  = np.load(os.path.join(directory, 'softmax_calc_w.npy'))
+#    preds_w = np.load(os.path.join(directory, 'softmax_preds_w.npy'))    
+
+def calculate_metrics(calc_w, preds_w, num_classes):
     pred_arg_maxs = np.argmax(preds_w, axis=1)
     calc_arg_maxs = np.argmax(calc_w, axis=1)
 
-    calc_pred_argmaxs_distances = calculate_errors_unsigned(calc_arg_maxs, pred_arg_maxs, num_classes)
+    calc_pred_argmaxs_abs_distances = calculate_errors_unsigned(calc_arg_maxs, pred_arg_maxs, num_classes)
+    calc_pred_argmaxs_signed_distances = calculate_errors_signed(calc_arg_maxs, pred_arg_maxs, num_classes)
     
-    print calc_pred_argmaxs_distances
-    print np.mean(calc_pred_argmaxs_distances), np.mean(calc_pred_argmaxs_distances) * 360./(1.0*num_classes)
+    print "abs_distances", calc_pred_argmaxs_abs_distances
+    print "signed_distances", calc_pred_argmaxs_signed_distances
+    print "mean distance", np.mean(calc_pred_argmaxs_signed_distances), np.mean(calc_pred_argmaxs_signed_distances) * 360./(1.0*num_classes),"[deg]"
 
-    mean_error = np.mean(calc_pred_argmaxs_distances)
+    mean_error = np.mean(calc_pred_argmaxs_signed_distances)
     #ERW: scaled to radians and in units of alpha^CP
     k2PI = 6.28
-    mean_error_scaled = np.mean(calc_pred_argmaxs_distances) * k2PI/(1.0*num_classes)
+    mean_error_scaled = np.mean(calc_pred_argmaxs_signed_distances) * k2PI/(1.0*num_classes)
     print mean_error_scaled
     
-    acc0 = (calc_pred_argmaxs_distances <= 0).mean()
-    acc1 = (calc_pred_argmaxs_distances <= 1).mean()
-    acc2 = (calc_pred_argmaxs_distances <= 2).mean()
-    acc3 = (calc_pred_argmaxs_distances <= 3).mean()
+    acc0 = (calc_pred_argmaxs_abs_distances <= 0).mean()
+    acc1 = (calc_pred_argmaxs_abs_distances <= 1).mean()
+    acc2 = (calc_pred_argmaxs_abs_distances <= 2).mean()
+    acc3 = (calc_pred_argmaxs_abs_distances <= 3).mean()
 
     l1_delta_w = np.mean(np.abs(calc_w - preds_w))
     l2_delta_w = np.sqrt(np.mean((calc_w - preds_w)**2))

anal_py/plot_softmax_rhorho_Variant_All.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from scipy import optimize
 
 
-pathIN  = "npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_10/"
+pathIN  = "../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_10/monit_npy/"
 pathOUT = "figures/"
 
 calc_w  = np.load(pathIN+'softmax_calc_w.npy')
@@ -48,7 +48,7 @@ plt.clf()
 #----------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------
 
-pathIN  = "npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_4/"
+pathIN  = "../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_4/monit_npy/"
 pathOUT = "figures/"
 
 calc_w_nc4  = np.load(pathIN+'softmax_calc_w.npy')
@@ -100,7 +100,7 @@ print('Acc3_nc_4', acc3)
 print('---------')
 #----------------------------------------------------------------------------------
 
-pathIN  = "npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_10/"
+pathIN  = "../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_10/monit_npy/"
 pathOUT = "figures/"
 
 calc_w_nc10  = np.load(pathIN+'softmax_calc_w.npy')
@@ -151,7 +151,7 @@ print('Acc3_nc_10', acc3)
 print('---------')
 #----------------------------------------------------------------------------------
 
-pathIN  = "npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_20/"
+pathIN  = "../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_20/monit_npy/"
 pathOUT = "figures/"
 
 calc_w_nc20  = np.load(pathIN+'softmax_calc_w.npy')
@@ -203,7 +203,7 @@ print('---------')
 #----------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------
 
-pathIN  = "npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_25/"
+pathIN  = "../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_25/monit_npy/"
 pathOUT = "figures/"
 
 calc_w_nc25  = np.load(pathIN+'softmax_calc_w.npy')
@@ -255,7 +255,7 @@ print('---------')
 #----------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------
 
-pathIN  = "npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_50/"
+pathIN  = "../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_50/monit_npy/"
 pathOUT = "figures/"
 
 calc_w_nc50  = np.load(pathIN+'softmax_calc_w.npy')
@@ -307,7 +307,7 @@ print('---------')
 #----------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------
 
-pathIN  = "npy/nn_rhorho_Variant-All_Unweighted_False_NO_NUM_CLASSES_100/"
+pathIN  = "../laptop_results/nn_rhorho_Variant-All_soft_Unweighted_False_NO_NUM_CLASSES_100/monit_npy/"
 pathOUT = "figures/"
 
 calc_w_nc100  = np.load(pathIN+'softmax_calc_w.npy')