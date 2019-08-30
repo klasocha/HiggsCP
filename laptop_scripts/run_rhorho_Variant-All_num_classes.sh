09e9d92f8b21dd3e5185dcba63a1ac28f2cc005b
elarichterwas <Elzbieta.Richter-Was@uj.edu.pl>
2019-08-30 10:23:41 +0200

    numclasses should be odd

--- laptop_scripts/run_rhorho_Variant-All_num_classes.sh
+++ laptop_scripts/run_rhorho_Variant-All_num_classes.sh
@@ -1,16 +1,16 @@
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 2
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 4
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 6
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 8
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 10
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 12
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 14
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 16
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 18
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 20
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 3
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 5
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 7
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 9
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 11
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 13
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 15
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 17
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 19
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 21
 
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 25
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 50
-python ../main.py -e 25 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 100
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 25
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 51
+python ../main.py -e 5 -t nn_rhorho -i $RHORHO_DATA -f Variant-All --num_classes 101
 
 
