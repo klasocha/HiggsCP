ddf835494f63ca00425d53be7137fa78f45ee56d
Janczar Kurek <janczar.kurek@student.uj.edu.pl>
2019-09-06 21:52:05 +0200

    [Refactor] File download refactor
    Removed duplicated code from file downloading.
    
    Signed-off-by: Janczar Kurek <janczar.kurek@student.uj.edu.pl>

--- src_py/download_data.py
+++ src_py/download_data.py
@@ -24,24 +24,3 @@ def download_files(filenames, data_path, merged_name=None):
     if merged_name is not None:
         merged_files = np.stack(merged_files)
         np.save(os.path.join(data_path, merged_name), merged_files)
-
-
-def download_data(args):
-    data_path = args.IN
-    if os.path.exists(data_path) is False:
-        os.mkdir(data_path)
-    download_weights(args)
-    download_data_files(args)
-
-
-def download_weights(args):
-    data_path = args.IN
-    CPmix_index = ['00', '02', '04', '06', '08', '10', '12', '14', '16', '18', '20']
-    filenames = ["rhorho_raw.w_{}.npy".format(cpmix_index) for cpmix_index in CPmix_index]
-    download_files(filenames, data_path, "rhorho_raw_w.npy")
-
-
-def download_data_files(args):
-    data_path = args.IN
-    files = ['rhorho_raw.data.npy', 'rhorho_raw.perm.npy']
-    download_files(files, data_path)
--- src_py/download_data_a1rho.py
+++ src_py/download_data_a1rho.py
@@ -1,14 +1,12 @@
 import os
-import urllib
 
-import numpy as np
+from download_data import download_files
 
-#DATA_URL = 'http://th-www.if.uj.edu.pl/~erichter/forHiggsCP_data/a1rho/'
+DATA_URL = 'http://th-www.if.uj.edu.pl/~erichter/forHiggsCP_data/a1rho/'
 
 
 def download_data(args):
     data_path = args.IN
-    data_path = "/home/erichter/Athena19/HiggsCPmix/branch-erichter-CPmix/HiggsCP_data/a1rho/"
     if os.path.exists(data_path) is False:
         os.mkdir(data_path)
     download_weights(args)
@@ -17,35 +15,12 @@ def download_data(args):
 
 def download_weights(args):
     data_path = args.IN
-    data_path = "/home/erichter/Athena19/HiggsCPmix/branch-erichter-CPmix/HiggsCP_data/a1rho/"
-    print data_path
-    # CPmix_index = 0 (scalar), 10 (pseudoscalar), 20 (scalar)
     CPmix_index = ['00', '02', '04', '06', '08', '10', '12', '14', '16', '18', '20']
-    weights = []
-    output_weight_file = os.path.join(data_path, 'a1rho_raw.w.npy')
-    if os.path.exists(output_weight_file) and not args.FORCE_DOWNLOAD:
-        print 'Output weights file exists. Downloading data cancelled. ' \
-              'If you want to force download use --force_download option'
-        return
-    for index in CPmix_index:
-        filename = 'a1rho_raw.w_' + index + '.npy'
-        print 'Donwloading ' + filename
-        filepath = os.path.join(data_path, filename)
-        #       urllib.urlretrieve(DATA_URL + filename, filepath)
-        weights.append(np.load(filepath))
-    weights = np.stack(weights)
-    np.save(output_weight_file, weights)
+    filenames = ["a1rho_raw.w_{}.npy".format(cpmix_index) for cpmix_index in CPmix_index]
+    download_files(filenames, data_path, "a1rho_raw_w.npy")
 
 
 def download_data_files(args):
     data_path = args.IN
-    data_path = "/home/erichter/Athena19/HiggsCPmix/branch-erichter-CPmix/HiggsCP_data/a1rho/"
     files = ['a1rho_raw.data.npy', 'a1rho_raw.perm.npy']
-    for file in files:
-        file_path = os.path.join(data_path, file)
-        if os.path.exists(file_path) and not args.FORCE_DOWNLOAD:
-            print 'File ' + file_path + ' exists. Downloading data cancelled. ' \
-                  'If you want to force download use --force_download option'
-        else:
-            print 'Donwloading ' + file
-            urllib.urlretrieve(DATA_URL + file, file_path)
+    download_files(files, data_path)
