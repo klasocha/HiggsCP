d1b976652bb1d22562a1a6e151b9cc117768c29c
Paulina Winkowska <paulina.winkowska@gmail.com>
2019-09-12 23:14:20 +0200

    Extracted fragments of code to separate functions in cpmix_utils

--- src_py/cpmix_utils.py
+++ src_py/cpmix_utils.py
@@ -7,6 +7,7 @@ from scipy import optimize
 def weight_fun(x, a, b, c):
     return a + b * np.cos(x) + c * np.sin(x)
 
+
 # here weights and arg_maxs are calculated from continuum distributions
 def calc_weights_and_arg_maxs(classes, popts, data_len, num_classes):
     arg_maxs = np.zeros(data_len)
@@ -30,13 +31,36 @@ def calc_weights_and_arg_maxs(classes, popts, data_len, num_classes):
     return weights, arg_maxs
 
 
+def calculate_popts_and_pcovs(weights, data_len, data_path):
+    popts = np.zeros((data_len, 3))
+    pcovs = np.zeros((data_len, 3, 3))
+    # here x correspond to values of CPmix at which data were generated
+    x = np.array([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2.0]) * np.pi
+    for i in range(data_len):
+        popt, pcov = optimize.curve_fit(weight_fun, x, weights[i, :], p0=[1, 1, 1])
+        popts[i] = popt
+        pcovs[i] = pcov
+
+    return popts, pcovs
+
+
+def should_calculate_popts(data_path):
+    return not os.path.exists(os.path.join(data_path, 'popts.npy'))
+
+
+def should_calculate_weights(reuse_weights, data_path, num_classes):
+    return not reuse_weights or not os.path.exists(os.path.join(data_path, 'weigths.npy')) \
+        or not os.path.exists(os.path.join(data_path, 'arg_maxs.npy')) \
+        or np.load(os.path.join(data_path, 'weigths.npy')).shape[1] != num_classes
+
+
 def preprocess_data(args):
     data_path = args.IN
     num_classes = args.NUM_CLASSES
     reuse_weigths = args.REUSE_WEIGTHS  # Set this flag to true if you want reuse calculated weights
 
     print "Loading data"
-    suffix = (args.TYPE).split("_")[-1] #-1 to indeks ostatniego elementu 
+    suffix = (args.TYPE).split("_")[-1]  # -1 to indeks ostatniego elementu
     data = read_np(os.path.join(data_path, suffix + "_raw.data.npy"))
     w = read_np(os.path.join(data_path, suffix + "_raw.w.npy")).swapaxes(0, 1)
     perm = read_np(os.path.join(data_path, suffix + "_raw.perm.npy"))
@@ -45,45 +69,37 @@ def preprocess_data(args):
     data_len = data.shape[0]
     classes = np.linspace(0, 2, num_classes) * np.pi
 
-    if not os.path.exists(os.path.join(data_path, 'popts.npy')):
-        popts = np.zeros((data_len, 3))
-        pcovs = np.zeros((data_len, 3, 3))
-        # here x correspond to values of CPmix at thich data were generated
-        x = np.array([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2.0]) * np.pi
-        for i in range(data_len):
-            popt, pcov = optimize.curve_fit(weight_fun, x, w[i, :], p0=[1, 1, 1])
-            popts[i] = popt
-            pcovs[i] = pcov
-
+    if should_calculate_popts(data_path):
+        popts, pcovs = calculate_popts_and_pcovs(w, data_len, data_path)
         np.save(os.path.join(data_path, 'popts.npy'), popts)
         np.save(os.path.join(data_path, 'pcovs.npy'), pcovs)
-    popts = np.load(os.path.join(data_path, 'popts.npy'))
+    else:
+        popts = np.load(os.path.join(data_path, 'popts.npy'))
 
-    if not reuse_weigths or not os.path.exists(os.path.join(data_path, 'weigths.npy')) \
-            or not os.path.exists(os.path.join(data_path, 'arg_maxs.npy')) \
-            or np.load(os.path.join(data_path, 'weigths.npy')).shape[1] != num_classes:
+    if should_calculate_weights(reuse_weigths, data_path, num_classes):
         weights, arg_maxs = calc_weights_and_arg_maxs(classes, popts, data_len, num_classes)
         np.save(os.path.join(data_path, 'weigths.npy'), weights)
         np.save(os.path.join(data_path, 'arg_maxs.npy'), arg_maxs)
-    weights  = np.load(os.path.join(data_path, 'weigths.npy'))
-    arg_maxs = np.load(os.path.join(data_path, 'arg_maxs.npy'))
+    else:
+        weights = np.load(os.path.join(data_path, 'weigths.npy'))
+        arg_maxs = np.load(os.path.join(data_path, 'arg_maxs.npy'))
 
-    #ERW
+    # ERW
     # here arg_maxs are in fraction of pi, not in the class index
     # how we go then from fraction of pi to class index??
     # print "preprocess: weights", weights
     # print "preprocess: arg_maxs", arg_maxs
 
-    #ERW
+    # ERW
     # I am not sure what the purpose is and if it make sens.
     if args.RESTRICT_MOST_PROBABLE_ANGLE:
         arg_maxs[arg_maxs > np.pi] = -1 * arg_maxs[arg_maxs > np.pi] + 2 * np.pi
 
-    #ERW
+    # ERW
     # this optimisation does not help, revisit, maybe not correctly implemented?
     if args.NORMALIZE_WEIGHTS:
-        weights = weights/np.reshape(popts[:, 0], (-1, 1))
-        
+        weights = weights / np.reshape(popts[:, 0], (-1, 1))
+
     # ERW    
     # here weights and arg_maxs are calculated at value of CPmix representing given class
     # in training, class is expressed as integer, not fraction pf pi.
