1e5d3afc683c8e9fa7f159373b01f39425827904
elarichterwas <Elzbieta.Richter-Was@uj.edu.pl>
2019-09-16 19:32:37 +0200

    corrected labels layout

--- plot_py/plot_popts_rhorho.py
+++ plot_py/plot_popts_rhorho.py
@@ -14,8 +14,8 @@ pathOUT = "figures/"
 def weight_fun(x, a, b, c):
     return a + b * np.cos(x) + c * np.sin(x)
 
-x_weights = np.array([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2]) * np.pi
-x_fit = np.linspace(0, 2*np.pi)
+x_weights = np.array([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2]) * 3.14
+x_fit = np.linspace(0, 6.28)
 
 #---------------------------------
 # ERW
@@ -25,19 +25,13 @@ x_fit = np.linspace(0, 2*np.pi)
 i = 0
 filename = "popts_rhorho_event_1"
 
-chi2 = np.sqrt(np.diag(pcovs[i]))
-
-plt.plot(x_weights, weights[:,i], 'o', label='generated')
-plt.plot(x_fit, weight_fun(x_fit, *popts[i]),label='fit')
+plt.plot(x_weights, weights[:,i], 'o', label="generated")
+plt.plot(x_fit, weight_fun(x_fit, *popts[i]), label="function")
 plt.ylim([0.0, 2.0])
 plt.xlabel(r'$\alpha^{CP}$ [rad]')
 plt.ylabel('wt')
+#plt.legend('loc=1')
 plt.legend()
-
-ax = plt.gca()
-#ERW
-# what is wrong with line below?
-#ax.annotate("chi2/Ndof = {%0.3f}\n".format(chi2), xy=(0.7, 0.85), xycoords='axes fraction', fontsize=12)
 plt.tight_layout()
 
 if filename:
@@ -55,9 +49,8 @@ else:
 
 print('error: ' + str(np.sqrt(np.diag(pcovs[i]))))
 
+#---------------------------------
 plt.clf()
-
-
 #---------------------------------
 
 i = 10 
@@ -98,17 +91,14 @@ plt.clf()
 i = 1000 
 filename = "popts_rhorho_event_1000"
 
-plt.plot(x_weights, weights[:,i], 'o')
-plt.plot(x_fit, weight_fun(x_fit, *popts[i]))
+plt.plot(x_weights, weights[:,i], 'o', label='generated')
+plt.plot(x_fit, weight_fun(x_fit, *popts[i]), label="function")
 plt.ylim([0.0, 2.0])
 plt.xlabel(r'$\alpha^{CP}$ [rad]')
 plt.ylabel('wt')
 plt.legend()
 
 ax = plt.gca()
-#ERW
-# what is wrong with line below?
-#ax.annotate("chi2/Ndof = {%0.3f}\n".format(chi2), xy=(0.7, 0.85), xycoords='axes fraction', fontsize=12)
 plt.tight_layout()
 
 if filename:
@@ -133,8 +123,8 @@ plt.clf()
 i = 2000 
 filename = "popts_rhorho_event_2000"
 
-plt.plot(x_weights, weights[:,i], 'o')
-plt.plot(x_fit, weight_fun(x_fit, *popts[i]))
+plt.plot(x_weights, weights[:,i], 'o', label='generated')
+plt.plot(x_fit, weight_fun(x_fit, *popts[i]),label='function')
 plt.ylim([0.0, 2.0])
 plt.xlabel(r'$\alpha^{CP}$ [rad]')
 plt.ylabel('wt')
