c204c26264e1e013d50be311a75c06e3177d01e8
elarichterwas <Elzbieta.Richter-Was@uj.edu.pl>
2019-11-09 20:28:47 +0100

    adjusting defintion of deltas_signed, deltas betwene last and first class is zero

--- src_py/metrics_utils.py
+++ src_py/metrics_utils.py
@@ -9,10 +9,12 @@ def calculate_deltas_unsigned(expected, actual, num_classes):
 
     return deltas
 
-
+# corrected, so the first and last class are the same class
+# OK for using in range (0, 2 pi) and trygonometric function
+# not OK otherwise!
 def calculate_deltas_signed(expected, actual, num_classes):
     deltas = actual - expected
-    deltas -= num_classes * (deltas > (num_classes // 2))
-    deltas += num_classes * (deltas <= (-num_classes // 2))
+    deltas -= (num_classes-1) * (deltas > (num_classes // 2))
+    deltas += (num_classes-1) * (deltas <= (-num_classes // 2))
 
     return deltas
