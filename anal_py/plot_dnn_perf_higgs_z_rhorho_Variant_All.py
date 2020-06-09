dea21b02bb082df6dad6336f30822100a5ff86f7
elarichterwas <Elzbieta.Richter-Was@uj.edu.pl>
2020-06-09 11:25:30 +0200

    added dir ana_py with plotting scripts, more output .npy stored in tf_model.py

---
+++ anal_py/plot_dnn_perf_higgs_z_rhorho_Variant_All.py
@@ -0,0 +1,80 @@
+import sys
+import os, errno
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+
+from scipy import optimize
+
+
+pathIN  = "../laptop_results/nn_higgs_z_rhorho_Variant-All/"
+pathOUT = "figures/"
+
+train_losses    = np.load(pathIN+'train_losses.npy')
+valid_losses    = np.load(pathIN+'valid_losses.npy')
+test_losses     = np.load(pathIN+'test_losses.npy')
+
+train_aucs      = np.load(pathIN+'train_aucs.npy')
+test_aucs       = np.load(pathIN+'test_aucs.npy')
+valid_aucs      = np.load(pathIN+'valid_aucs.npy')
+
+#----------------------------------------------------------------------------------
+
+filename = "dnn_train_loss_higgs_z_rhorho_Variant-All"
+x = np.arange(1,len(train_losses)+1)
+plt.plot([0], marker='None',
+           linestyle='None', label=r'Classification: Higgs vs Z')
+plt.plot(x,train_losses, 'o', color = 'black', label='Training')
+plt.plot(x,valid_losses, 'd', color = 'orange', label='Validation')
+plt.legend()
+plt.ylim([0.725, 0.800])
+plt.xlabel('Number of epochs')
+plt.xticks(x)
+plt.ylabel('Loss')
+#plt.title('Features list: Variant-All')
+    
+if filename:
+    try:
+        os.makedirs(pathOUT)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    plt.savefig(pathOUT + filename+".eps")
+    print('Saved '+pathOUT + filename+".eps")
+    plt.savefig(pathOUT + filename+".pdf")
+    print('Saved '+pathOUT + filename+".pdf")
+else:
+    plt.show()
+plt.clf()
+
+
+#----------------------------------------------------------------------------------
+
+filename = "dnn_train_aucs_higgs_z_rhorho_Variant-All"
+x = np.arange(1,len(train_aucs)+1)
+plt.plot(x,train_aucs, 'o', label='training')
+plt.plot(x,valid_aucs, 'd', label='validation')
+plt.legend()
+#plt.ylim([0.0, 0.4])
+plt.xlabel('Number of epochs')
+plt.xticks(x)
+plt.ylabel(r'aucs')
+#plt.title('Features list: Variant-All')
+    
+if filename:
+    try:
+        os.makedirs(pathOUT)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    plt.savefig(pathOUT + filename+".eps")
+    print('Saved '+pathOUT + filename+".eps")
+    plt.savefig(pathOUT + filename+".pdf")
+    print('Saved '+pathOUT + filename+".pdf")
+else:
+    plt.show()
+plt.clf()
+
+
+#----------------------------------------------------------------------------------
---
+++ anal_py/plot_dnn_roc_higgs_z_rhorho_Variant_All.py
@@ -0,0 +1,46 @@
+import sys
+import os, errno
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import auc
+
+pathIN  =  "../laptop_results/nn_higgs_z_rhorho_Variant-All/"
+pathOUT =  "figures/"
+
+roc_curve_data = np.load(pathIN+'roc_curve_data.npy')
+fpr = roc_curve_data[0]
+tpr = roc_curve_data[1]
+auc_score =  auc(fpr, tpr)
+
+
+#----------------------------------------------------------------------------------
+
+filename = "dnn_roc_curve_higgs_z_rhorho_Variant-All"
+
+plt.figure()
+lw = 2
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % auc_score)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+#plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+    
+if filename:
+    try:
+        os.makedirs(pathOUT)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    plt.savefig(pathOUT + filename+".eps")
+    print('Saved '+pathOUT + filename+".eps")
+    plt.savefig(pathOUT + filename+".pdf")
+    print('Saved '+pathOUT + filename+".pdf")
+else:
+    plt.show()
+plt.clf()
+
+#----------------------------------------------------------------------------------
