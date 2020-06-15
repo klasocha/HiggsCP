190de9a4985d33db841b8a8bd30e7d76c220c55e
elarichterwas <Elzbieta.Richter-Was@uj.edu.pl>
2020-06-15 11:50:44 +0200

    added option narrow mass window Z sample

--- laptop_scripts/run_25epochs.sh
+++ laptop_scripts/run_25epochs.sh
@@ -1,4 +1,9 @@
-python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-All
-python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-1.0
-python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-5.0
-python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-5.1
+python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-All --Zsample z115
+python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-1.0 --Zsample z115
+python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-5.0 --Zsample z115
+python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-5.1 --Zsample z115
+
+python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-All --Zsample z65
+python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-1.0 --Zsample z65
+python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-5.0 --Zsample z65
+python ../main.py -e 25 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-5.1 --Zsample z65
--- laptop_scripts/run_higgs_z_rhorho_Variant-All.sh
+++
@@ -1,1 +0,0 @@
-python ../main.py -e 5 -t nn_higgs_z_rhorho -i $RHORHO_DATA -f Variant-All -d 0.0 
