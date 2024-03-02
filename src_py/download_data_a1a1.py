ab5f333f9c09406e5e2c6f24bdbbd75b90e45199
Tair Yerniyazov <40953080+TairYerniyazov@users.noreply.github.com>
2024-03-02 01:43:50 +0100

    Refactor src_py/download_data_a1*.py

--- src_py/download_data_a1a1.py
+++ src_py/download_data_a1a1.py
@@ -1,6 +1,5 @@
 import os
-import urllib
-
+from urllib.request import urlretrieve 
 import numpy as np
 
 DATA_URL = 'http://th-www.if.uj.edu.pl/~erichter/forHiggsCP_data/a1a1'
@@ -21,14 +20,14 @@ def download_weights(args):
     weights = []
     output_weight_file = os.path.join(data_path, 'a1a1_raw.w.npy')
     if os.path.exists(output_weight_file) and not args.FORCE_DOWNLOAD:
-        print 'Output weights file exists. Downloading data cancelled. ' \
-              'If you want to force download use --force_download option'
+        print('Output weights file exists. Downloading data cancelled. ',
+              'If you want to force download use --force_download option')
         return
     for index in CPmix_index:
         filename = 'a1a1_raw.w_' + index + '.npy'
-        print 'Donwloading ' + filename
+        print('Donwloading ', filename)
         filepath = os.path.join(data_path, filename)
-#        urllib.urlretrieve(DATA_URL + filename, filepath)
+        # urlretrieve(DATA_URL + filename, filepath)
         weights.append(np.load(filepath))
     weights = np.stack(weights)
     np.save(output_weight_file, weights)
@@ -40,8 +39,8 @@ def download_data_files(args):
     for file in files:
         file_path = os.path.join(data_path, file)
         if os.path.exists(file_path) and not args.FORCE_DOWNLOAD:
-            print 'File ' + file_path + ' exists. Downloading data cancelled. ' \
-                  'If you want to force download use --force_download option'
+            print('File ' + file_path + ' exists. Downloading data cancelled. ',
+                  'If you want to force download use --force_download option')
         else:
-            print 'Donwloading ' + file
-            urllib.urlretrieve(DATA_URL + file, file_path)
+            print('Donwloading ', file)
+            urlretrieve(DATA_URL + file, file_path)
--- src_py/download_data_a1rho.py
+++ src_py/download_data_a1rho.py
@@ -1,9 +1,8 @@
 import os
-import urllib
-
+from urllib.request import urlretrieve 
 import numpy as np
 
-#DATA_URL = 'http://th-www.if.uj.edu.pl/~erichter/forHiggsCP_data/a1rho/'
+DATA_URL = 'http://th-www.if.uj.edu.pl/~erichter/forHiggsCP_data/a1rho/'
 
 
 def download_data(args):
@@ -18,20 +17,20 @@ def download_data(args):
 def download_weights(args):
     data_path = args.IN
     data_path = "/home/erichter/Athena19/HiggsCPmix/branch-erichter-CPmix/HiggsCP_data/a1rho/"
-    print data_path
+    print(data_path)
     # CPmix_index = 0 (scalar), 10 (pseudoscalar), 20 (scalar)
     CPmix_index = ['00', '02', '04', '06', '08', '10', '12', '14', '16', '18', '20']
     weights = []
     output_weight_file = os.path.join(data_path, 'a1rho_raw.w.npy')
     if os.path.exists(output_weight_file) and not args.FORCE_DOWNLOAD:
-        print 'Output weights file exists. Downloading data cancelled. ' \
-              'If you want to force download use --force_download option'
+        print('Output weights file exists. Downloading data cancelled. ',
+              'If you want to force download use --force_download option')
         return
     for index in CPmix_index:
         filename = 'a1rho_raw.w_' + index + '.npy'
-        print 'Donwloading ' + filename
+        print('Donwloading ', filename)
         filepath = os.path.join(data_path, filename)
-        #       urllib.urlretrieve(DATA_URL + filename, filepath)
+        # urlretrieve(DATA_URL + filename, filepath)
         weights.append(np.load(filepath))
     weights = np.stack(weights)
     np.save(output_weight_file, weights)
@@ -44,8 +43,8 @@ def download_data_files(args):
     for file in files:
         file_path = os.path.join(data_path, file)
         if os.path.exists(file_path) and not args.FORCE_DOWNLOAD:
-            print 'File ' + file_path + ' exists. Downloading data cancelled. ' \
-                  'If you want to force download use --force_download option'
+            print('File ' + file_path + ' exists. Downloading data cancelled. ',
+                  'If you want to force download use --force_download option')
         else:
-            print 'Donwloading ' + file
-            urllib.urlretrieve(DATA_URL + file, file_path)
+            print('Donwloading ', file)
+            urlretrieve(DATA_URL + file, file_path)
