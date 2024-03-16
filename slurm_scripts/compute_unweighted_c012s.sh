6acebc7053e5ace1460b8f7460613c06b4aefefd
Tair Yerniyazov <40953080+TairYerniyazov@users.noreply.github.com>
2024-03-17 00:34:49 +0100

    Change hardware specification

--- slurm_scripts/compute_unweighted_c012s.sh
+++ slurm_scripts/compute_unweighted_c012s.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH -job-name=Compute_C012_for_all_hypotheses
-#SBATCH --nodes=11
+
+#SBATCH --job-name=Compute_C012_for_all_hypotheses
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=3850M
@@ -9,9 +10,10 @@
 #SBATCH --array=0-10
 #SBATCH --output="results/output-%A_%a.out"
 #SBATCH --error="results/error-%A_%a.err"
+
 cd $SLURM_SUBMIT_DIR
-## Command
+
 HYPOTHESES=(00 02 04 06 08 10 12 14 16 18 20)
 ALPHA_CP_CLASS=${HYPOTHESES[$SLURM_ARRAY_TASK_ID]}
-echo "Computing the C0/C1/C2 coefficients for the hypothesis specified by alphaCP=" $ALPHA_CP_CLASS
+echo "Computing the C0/C1/C2 coefficients for the hypothesis specified by alphaCP =" $ALPHA_CP_CLASS
 python unweight_events.py --input "data" --option "PREPARE-C012S" --hypothesis $ALPHA_CP_CLASS
