9add824a01501f387ae3c949c124bde2fd18725a
Tair Yerniyazov <40953080+TairYerniyazov@users.noreply.github.com>
2024-03-19 03:13:01 +0100

    Change descriptions ("plot" <- "diagram")

--- plots.py
+++ plots.py
@@ -13,15 +13,15 @@ types = {"PHISTAR-DISTRIBUTION" : phistar_dist, # Variant-1.1 should be prepared
          "C012S-WEIGHT" : c012s_weight,
          "C012S-DISTRIBUTION" : c012s_dist,
          "WEIGHTS-FOR-EVENT-VIA-C012": weights_with_c012s}
-parser = argparse.ArgumentParser(description='Diagram creator')
+parser = argparse.ArgumentParser(description='Plot creator')
 parser.add_argument("-i", "--input", dest="IN", type=Path, help="input data path", default="../temp_data")
-parser.add_argument("-o", "--output", dest="OUT", type=Path, help="output path for diagrams", default="figures")
+parser.add_argument("-o", "--output", dest="OUT", type=Path, help="output path for plots", default="figures")
 parser.add_argument("-f", "--format", dest="FORMAT", 
-                    help='the format of the output diagrams ("png"/"pdf"/"eps")', default="png")
+                    help='the format of the output plots ("png"/"pdf"/"eps")', default="png")
 parser.add_argument("-s", "--show", dest="SHOW", action="store_true", 
-                    help='set to False to save the diagrams without showing them', default=False)
+                    help='set to False to save the plots without showing them', default=False)
 parser.add_argument("--option", dest="OPTION", choices=types.keys(), default="PHISTAR-DISTRIBUTION",
-                    help='specify what script for drawing the diagrams you want to run', required=True)
+                    help='specify what script for drawing the plots you want to run', required=True)
 parser.add_argument("--hypothesis", dest="HYPOTHESIS", default="None", 
                     help="Hypothesis: the alphaCP class (e.g. 02)")
 
