6a8e23772f200297bb0eb386dc77f3eb348c3bd1
Tair Yerniyazov <40953080+TairYerniyazov@users.noreply.github.com>
2024-03-19 01:57:17 +0100

    Rollback the script plotting the weights via C0/C1/C2 and remove the slurm script created earlier

--- plot_py/plot_weights_with_c012s.py
+++ plot_py/plot_weights_with_c012s.py
@@ -6,13 +6,6 @@ and you want to save the resul in "plot_py/figures/"):
 
     $ python plots.py --option WEIGHTS-FOR-EVENT-VIA-C012 --input "data" 
       --output "plot_py/figures" --format "png" --show 
-
-    BETA-VERSION: if --use_unweighted_events:
-    ======================================================================================
-    $ python plots.py --option WEIGHTS-FOR-EVENT-VIA-C012 --input "data" 
-      --output "plot_py/figures" --format "png" --show --use_unweighted_events 
-      --hypothesis 00
-    ======================================================================================
 """
 
 import numpy as np
@@ -32,16 +25,8 @@ def draw(args):
     # Sample events indices
     sample_events = [4, 2569, 8533, 55, 995]
 
-    if args.USE_UNWEIGHTED_EVENTS:
-        # Reading the unweighted events coefficients for a given hypotheses
-        c012s = read_np(os.path.join(args.IN, f"unweighted_c012s_{args.HYPOTHESIS}.npy"))
-        c012s = np.mean(c012s, axis=0)
-        # Shape modifications for backward compatibility (use_unweighted_events option was added later):
-        sample_events = [0]
-        c012s = np.array([c012s]) * 20
-    else:
-        # Reading the initial events coefficients
-        c012s = read_np(os.path.join(args.IN, "c012s.npy"))
+    # Reading the initial events coefficients
+    c012s = read_np(os.path.join(args.IN, "c012s.npy"))
 
     alphaCP_range = np.linspace(0, 2 * np.pi, n_discrete_values)
     calculated_weights = []
@@ -56,28 +41,18 @@ def draw_distribution(weights, c0, alphaCP_range, args):
     for i in range(len(weights)):
         # Weights calculated via C0/C1/C2
         ax = axs[0]
-        if not args.USE_UNWEIGHTED_EVENTS:
-            ax.set_ylim(0, 2)
-            label = f"Event {i + 1}"
-            color = None
-        else:
-            label = f"Based on the unweighted \nevents for alphaCP = {args.HYPOTHESIS}"
-            color = "black"
-        ax.scatter(alphaCP_range, weights[i], s=19, marker=markers[i], label=label, color=color)
+        ax.set_ylim(0, 2)
+        label = f"Event {i + 1}"
+        ax.scatter(alphaCP_range, weights[i], s=19, marker=markers[i], label=label)
         ax.legend(loc='lower right')
         ax.set_ylabel(r"$C_0 + C_1 cos({\alpha}^{CP}) + C_2 sin({\alpha^{CP}})$", loc="top")
         ax.set_xlabel(r"${\alpha^{CP}}$ [rad]", loc="right")
         
         # Weights calculated via C1/C2
         ax = axs[1]
-        if not args.USE_UNWEIGHTED_EVENTS:
-            ax.set_ylim(-1, 1)
-            label = f"Event {i + 1}"
-            color = None
-        else:
-            color = "black"
-            label = f"Based on the unweighted \nevents for alphaCP={args.HYPOTHESIS}"
-        ax.scatter(alphaCP_range, weights[i] - c0[i], s=19, marker=markers[i], label=label, color=color)
+        ax.set_ylim(-1, 1)
+        label = f"Event {i + 1}"
+        ax.scatter(alphaCP_range, weights[i] - c0[i], s=19, marker=markers[i], label=label)
         ax.legend(loc='lower right')
         ax.set_ylabel(r"$C_1 cos({\alpha}^{CP}) + C_2 sin({\alpha^{CP}})$", loc="top")
         ax.set_xlabel(r"${\alpha^{CP}}$ [rad]", loc="right")
@@ -93,11 +68,7 @@ def draw_distribution(weights, c0, alphaCP_range, args):
             raise
 
     # Saving the diagram
-    if args.USE_UNWEIGHTED_EVENTS:
-        output_path = os.path.join(output_path, f"unweighted_c012s_vs_c12s_{args.HYPOTHESIS}.{args.FORMAT}")
-    else:
-        output_path = os.path.join(output_path, f"weights_c012s_vs_c12s.{args.FORMAT}")
-    
+    output_path = os.path.join(output_path, f"weights_c012s_vs_c12s.{args.FORMAT}")
     plt.savefig(output_path)
     print(f"Wt(alphaCP) subplots have been saved in {output_path}")
 
