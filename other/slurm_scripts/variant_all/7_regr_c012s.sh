83efd837c9ecb407e6a5da288a5b8799c55fcbaf
yerniyazov_tair <tair.yerniyazov@student.uj.edu.pl>
2024-05-09 19:55:28 +0200

    Add slurm scripts for Variant-All

---
+++ other/slurm_scripts/variant_all/7_regr_c012s.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=variant_all_regr_c012s
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=9216Mb
+#SBATCH --time=02:30:00
+#SBATCH --output="results/slurm_scripts_logs/output-%A_%a.out"
+#SBATCH --error="results/slurm_scripts_logs/error-%A_%a.err"
+
+cd $SLURM_SUBMIT_DIR
+module load python/3.11.3-gcccore-12.3.0
+source .venv/bin/activate
+    	
+python main.py --action "train" --input "data" --num_classes "51" --epochs "120" --training_method "regr_c012s" --model_location "51_classes_variant_all"
+python main.py --action "predict_test" --input "data" --num_classes "51" --training_method "regr_c012s" --model_location "51_classes_variant_all" --use_filtered_data
+
+python main.py --action "plot" --input "results/regr_c012s/51_classes_variant_all/predictions" --output "plots/figures" --format "png" --option "RESULTS_ANALYSIS_4" --num_classes "51" --training_method "regr_c012s" --features Variant-All --dataset "test" --use_filtered_data
+python main.py --action "plot" --input "results/regr_c012s/51_classes_variant_all/predictions" --output "plots/figures" --format "pdf" --option "RESULTS_ANALYSIS_4" --num_classes "51" --training_method "regr_c012s" --features Variant-All --dataset "test" --use_filtered_data
+python main.py --action "plot" --input "results/regr_c012s/51_classes_variant_all/predictions" --output "plots/figures" --format "eps" --option "RESULTS_ANALYSIS_4" --num_classes "51" --training_method "regr_c012s" --features Variant-All --dataset "test" --use_filtered_data
+
+deactivate
