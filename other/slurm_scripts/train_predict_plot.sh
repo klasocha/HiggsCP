003c30a679060f507075f6b439d2e69e6734f246
yerniyazov_tair <tair.yerniyazov@student.uj.edu.pl>
2024-05-11 15:17:25 +0200

    Small fix

--- other/slurm_scripts/train_predict_plot.sh
+++ other/slurm_scripts/train_predict_plot.sh
@@ -2,7 +2,7 @@
 
 export EPOCHS=120
 
-# JOBID0=$(sbatch --parsable -p plgrid other/slurm_scripts/variant_1.1/0_prepare_data.sh)
+JOBID0=$(sbatch --parsable -p plgrid other/slurm_scripts/variant_1.1/0_prepare_data.sh)
 sbatch --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/variant_1.1/1_train_soft_weights.sh
 JOBID2=$(sbatch --parsable --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/variant_1.1/2_soft_c012s_c0.sh)
 JOBID3=$(sbatch --parsable --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/variant_1.1/3_soft_c012s_c1.sh)
@@ -12,7 +12,7 @@ sbatch --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/variant_1.1/6_
 sbatch --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/variant_1.1/7_regr_c012s.sh
 sbatch --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/variant_1.1/8_regr_argmaxs.sh
 
-# JOBID4=$(sbatch --parsable -p plgrid other/slurm_scripts/variant_all/0_prepare_data.sh)
+JOBID4=$(sbatch --parsable -p plgrid other/slurm_scripts/variant_all/0_prepare_data.sh)
 sbatch --dependency afterok:$JOBID4 -p plgrid other/slurm_scripts/variant_all/1_train_soft_weights.sh
 JOBID5=$(sbatch --parsable --dependency afterok:$JOBID4 -p plgrid other/slurm_scripts/variant_all/2_soft_c012s_c0.sh)
 JOBID6=$(sbatch --parsable --dependency afterok:$JOBID4 -p plgrid other/slurm_scripts/variant_all/3_soft_c012s_c1.sh)
