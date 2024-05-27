3b43672a3bfdef5ff354c947495dd0019646f954
Tair Yerniyazov <40953080+TairYerniyazov@users.noreply.github.com>
2024-05-27 22:49:23 +0200

    Update Chi2

--- tests/test_unwt_evt_1.py
+++ tests/test_unwt_evt_1.py
@@ -154,7 +154,7 @@ def test_on_unwt_events(args):
     summed_true_wt = np.sum(true_weights, axis=0)
     min_summed_wt, max_summed_wt = np.min(summed_wt), np.max(summed_wt)
     relative_amplitude = 2 * (max_summed_wt - min_summed_wt) / (max_summed_wt + min_summed_wt)
-    chi2_nf = np.sum(np.square(summed_true_wt - summed_wt)) / discr_level 
+    chi2_nf = np.sum(np.square(summed_true_wt - summed_wt) / summed_true_wt) / discr_level 
 
     draw_distribution(
         x=np.roll(np.arange(0, discr_level - 1), int((discr_level - 1) / 2)),
