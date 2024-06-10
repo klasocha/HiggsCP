fae598acfc526d5e2f5173cca11fea814b5dbc72
yerniyazov_tair <tair.yerniyazov@student.uj.edu.pl>
2024-06-10 16:44:42 +0200

    Update across the preprocessing steps to compy with Z-background data

--- utilities/cpmix_utils.py
+++ utilities/cpmix_utils.py
@@ -179,19 +179,20 @@ def preprocess_data(args):
     argmaxs = read_np(argmaxs_path)
     hits_argmaxs = read_np(hits_argmaxs_path)
 
-    # Unweighting the events and saving the "hits"
-    unweighted_events_weights_filename = f"unwt_multiclass_{num_classes}.npy"
-    weights_normalised = weights / 2
-    data_len = len(weights_normalised)
-    unweighted_events = []
-    monte_carlo = lambda x : 0.0 if x < np.random.random() else 1.0
+    if args.EXP != "Z":
+        # Unweighting the events and saving the "hits"
+        unweighted_events_weights_filename = f"unwt_multiclass_{num_classes}.npy"
+        weights_normalised = weights / 2
+        data_len = len(weights_normalised)
+        unweighted_events = []
+        monte_carlo = lambda x : 0.0 if x < np.random.random() else 1.0
 
-    print(f"Unweighting the events...", end='\r')
-    unweighted_events = np.vectorize(monte_carlo)(weights_normalised)
-    
-    output_path = os.path.join(data_path, unweighted_events_weights_filename)
-    np.save(output_path, unweighted_events)
-    print(f"Weights of the unweighted events have been saved in {output_path}")
+        print(f"Unweighting the events...", end='\r')
+        unweighted_events = np.vectorize(monte_carlo)(weights_normalised)
+        
+        output_path = os.path.join(data_path, unweighted_events_weights_filename)
+        np.save(output_path, unweighted_events)
+        print(f"Weights of the unweighted events have been saved in {output_path}")
 
     # TODO: Revisit
     # Comment from ERW:
--- utilities/prepare_data.py
+++ utilities/prepare_data.py
@@ -13,8 +13,9 @@ def prepare_data(args):
     num_classes = args.NUM_CLASSES
 
     # ==================================== DATA PREPARATION ============================================
-    print("\033[1mDownloading data...\033[0m")
-    download_data(args)
+    if args.EXP != "Z":
+        print("\033[1mDownloading data...\033[0m")
+        download_data(args)
     
     print("\033[1mPreprocessing data...\033[0m")
     data, weights, argmaxs, perm, c012s, hits_argmaxs, hits_c012s = preprocess_data(args)
