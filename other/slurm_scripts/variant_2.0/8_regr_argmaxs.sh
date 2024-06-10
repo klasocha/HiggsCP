a59a446277c39d8a6d00faa9677a7e91569947b0
yerniyazov_tair <tair.yerniyazov@student.uj.edu.pl>
2024-06-10 02:56:22 +0200

    Add Slurm scripts for Variant-2.0/2.1

---
+++ other/slurm_scripts/variant_2.0/8_regr_argmaxs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH --job-name=variant_2.0_regr_argmaxs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=9216Mb
+#SBATCH --time=06:00:00
+#SBATCH --output="results/slurm_scripts_logs/output-%A_%a.out"
+#SBATCH --error="results/slurm_scripts_logs/error-%A_%a.err"
+
+cd $SLURM_SUBMIT_DIR
+module load python/3.11.3-gcccore-12.3.0
+source .venv/bin/activate
+    	
+# Training
+python main.py --action "train" --input "data" --num_classes "51" --epochs $EPOCHS \
+--training_method "regr_argmaxs" --model_location "51_classes_variant_2.0" \
+--features Variant-2.0 --keras "v2"
+
+# Prediction (test)
+python main.py --action "predict_test" --input "data" --num_classes "51" \
+--training_method "regr_argmaxs" --model_location "51_classes_variant_2.0" \
+--features Variant-2.0 --use_filtered_data --keras "v2"
+python main.py --action "predict_test" --input "data" --num_classes "51" \
+--training_method "regr_argmaxs" --model_location "51_classes_variant_2.0" \
+--features Variant-2.0 --keras "v2"
+
+# Prediction (train_and_valid)
+python main.py --action "predict_train_and_valid" --input "data" --num_classes "51" \
+--training_method "regr_argmaxs" --model_location "51_classes_variant_2.0" \
+--features Variant-2.0 --use_filtered_data --keras "v2"
+python main.py --action "predict_train_and_valid" --input "data" --num_classes "51" \
+--training_method "regr_argmaxs" --model_location "51_classes_variant_2.0" \
+--features Variant-2.0 --keras "v2"
+
+deactivate
