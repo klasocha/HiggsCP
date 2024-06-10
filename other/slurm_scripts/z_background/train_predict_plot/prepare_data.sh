aaa997dfcb7b96face55c5173b7a9d1cad252bec
yerniyazov_tair <tair.yerniyazov@student.uj.edu.pl>
2024-06-10 18:10:41 +0200

    Update Slurm scipts for the Z-background experiment

--- other/slurm_scripts/z_background/train_predict_plot/prepare_data.sh
+++ other/slurm_scripts/z_background/train_predict_plot/prepare_data.sh
@@ -14,6 +14,7 @@ module load python/3.11.3-gcccore-12.3.0
 source .venv/bin/activate
 
 mkdir data_z
-python main.py --action "download_and_prepare_original" --input "data_z" --exp "Z"
 scp data/unwt_multiclass_51.npy data_z/
+python main.py --action "download_and_prepare_original" --input "data_z" --exp "Z"
+
 deactivate
