9c8393b5802e37c440852e8f204b5b33b11f5c6c
yerniyazov_tair <tair.yerniyazov@student.uj.edu.pl>
2024-06-10 22:22:22 +0200

    Update Slurm scripts

---
+++ other/slurm_scripts/z_background/all_events_variant_2.1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#SBATCH --job-name=variant_2.1_z_all_evt
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=9216Mb
+#SBATCH --time=02:00:00
+#SBATCH --output="results/slurm_scripts_logs/output-%A_%a.out"
+#SBATCH --error="results/slurm_scripts_logs/error-%A_%a.err"
+
+cd $SLURM_SUBMIT_DIR
+module load python/3.11.3-gcccore-12.3.0
+source .venv/bin/activate
+
+# Creating plots (all events with DNNs trained on Higgs events)
+# Variant-2.1
+python main.py --action "test_model_on_all_events" --input "data_z" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_2.1/soft_weights" --num_classes "51" \
+--training_method "soft_weights" --model_location "51_classes_variant_2.1" \
+--features "Variant-2.1"
+
+python main.py --action "test_model_on_all_events" --input "data_z" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_2.1/regr_weights" --num_classes "51" \
+--training_method "regr_weights" --model_location "51_classes_variant_2.1" \
+--features "Variant-2.1"
+
+python main.py --action "test_model_on_all_events" --input "data_z" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_2.1/soft_c012s" --num_classes "51" \
+--training_method "soft_c012s" --model_location "variant_2.1/51_classes" \
+--features "Variant-2.1"
+
+python main.py --action "test_model_on_all_events" --input "data_z" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_2.1/regr_c012s" --num_classes "51" \
+--training_method "regr_c012s" --model_location "51_classes_variant_2.1" \
+--features "Variant-2.1"
+
+python main.py --action "test_model_on_all_events" --input "data_z" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_2.1/soft_argmaxs" --num_classes "51" \
+--training_method "soft_argmaxs" --model_location "51_classes_variant_2.1" \
+--features "Variant-2.1"
+
+python main.py --action "test_model_on_all_events" --input "data_z" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_2.1/regr_argmaxs" --num_classes "51" \
+--training_method "regr_argmaxs" --model_location "51_classes_variant_2.1" \
+--features "Variant-2.1"
+
+deactivate
--- other/slurm_scripts/z_background/prepare_data.sh
+++ other/slurm_scripts/z_background/prepare_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=variant_1.0_soft_argmaxs
+#SBATCH --job-name=variant_1.0_data_preparation
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=9216Mb
-#SBATCH --time=06:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output="results/slurm_scripts_logs/output-%A_%a.out"
 #SBATCH --error="results/slurm_scripts_logs/error-%A_%a.err"
 
@@ -13,9 +13,9 @@ cd $SLURM_SUBMIT_DIR
 module load python/3.11.3-gcccore-12.3.0
 source .venv/bin/activate
 
-# Training
-python main.py --action "train" --input "data_z" --num_classes "51" --epochs $EPOCHS \
---training_method "soft_argmaxs" --model_location "51_classes_variant_1.0" \
---features Variant-1.0
+# Preparing data
+mkdir data_z
+scp data/unwt_multiclass_51.npy data_z/
+python main.py --action "download_and_prepare_original" --input "data_z" --exp "Z"
 
 deactivate
