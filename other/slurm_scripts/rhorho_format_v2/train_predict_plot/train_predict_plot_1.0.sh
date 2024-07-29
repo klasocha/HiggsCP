b7491d9c96566d521e8d24d29c4d80ab1315b8cd
Tair Yerniyazov <40953080+TairYerniyazov@users.noreply.github.com>
2024-07-29 22:37:53 +0200

    Add Slurm scripts for training models on the data prepared in the Run 2 format

---
+++ other/slurm_scripts/rhorho_format_v2/train_predict_plot/train_predict_plot_1.0.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export EPOCHS=25
+export EXP=RhoRho
+
+JOBID0=$(sbatch --parsable -p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/0_prepare_data.sh)
+JOBID1=$(sbatch --parsable --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/1_soft_weights.sh)
+JOBID2=$(sbatch --parsable --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/2_soft_c012s_c0.sh)
+JOBID3=$(sbatch --parsable --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/3_soft_c012s_c1.sh)
+JOBID4=$(sbatch --parsable --dependency afterok:$JOBID0:$JOBID2:$JOBID3 -p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/4_soft_c012s_c2.sh)
+JOBID5=$(sbatch --parsable --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/5_soft_argmaxs.sh)
+JOBID6=$(sbatch --parsable --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/6_regr_weights.sh)
+JOBID7=$(sbatch --parsable --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/7_regr_c012s.sh)
+JOBID8=$(sbatch --parsable --dependency afterok:$JOBID0 -p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/8_regr_argmaxs.sh)
+JOBID9=$(sbatch --parsable --dependency \
+afterok:$JOBID0:$JOBID1:$JOBID2:$JOBID3:$JOBID4:$JOBID5:$JOBID6:$JOBID7:$JOBID8 \
+-p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/9_results_analysis.sh)
+JOBID10=$(sbatch --parsable --dependency \
+afterok:$JOBID0:$JOBID1:$JOBID2:$JOBID3:$JOBID4:$JOBID5:$JOBID6:$JOBID7:$JOBID8 \
+-p plgrid other/slurm_scripts/rhorho/train_predict_plot/variant_1.0/10_test_evt.sh)
---
+++ other/slurm_scripts/rhorho_format_v2/variant_1.1/10_test_evt.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#SBATCH --job-name=variant_1.1_results_analysis
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=9216Mb
+#SBATCH --time=06:00:00
+#SBATCH --output="results/slurm_scripts_logs/output-%A_%a.out"
+#SBATCH --error="results/slurm_scripts_logs/error-%A_%a.err"
+
+cd $SLURM_SUBMIT_DIR
+module load python/3.11.3-gcccore-12.3.0
+source .venv/bin/activate
+
+# Unweighted events test
+python main.py --action "test_model_on_unwt_events" --input "data" \
+--output "plots/figures/test_model_on_unwt_events/51_classes_variant_1.1/soft_weights" --num_classes "51" \
+--hypothesis "0-4-46" --training_method "soft_weights" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_unwt_events" --input "data" \
+--output "plots/figures/test_model_on_unwt_events/51_classes_variant_1.1/regr_weights" --num_classes "51" \
+--hypothesis "0-4-46" --training_method "regr_weights" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_unwt_events" --input "data" \
+--output "plots/figures/test_model_on_unwt_events/51_classes_variant_1.1/soft_c012s" --num_classes "51" \
+--hypothesis "0-4-46" --training_method "soft_c012s" --model_location "variant_1.1/51_classes" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_unwt_events" --input "data" \
+--output "plots/figures/test_model_on_unwt_events/51_classes_variant_1.1/regr_c012s" --num_classes "51" \
+--hypothesis "0-4-46" --training_method "regr_c012s" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_unwt_events" --input "data" \
+--output "plots/figures/test_model_on_unwt_events/51_classes_variant_1.1/soft_argmaxs" --num_classes "51" \
+--hypothesis "0-4-46" --training_method "soft_argmaxs" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_unwt_events" --input "data" \
+--output "plots/figures/test_model_on_unwt_events/51_classes_variant_1.1/regr_argmaxs" --num_classes "51" \
+--hypothesis "0-4-46" --training_method "regr_argmaxs" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+
+# All events test
+python main.py --action "test_model_on_all_events" --input "data" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_1.1/soft_weights" --num_classes "51" \
+--training_method "soft_weights" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_all_events" --input "data" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_1.1/regr_weights" --num_classes "51" \
+--training_method "regr_weights" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_all_events" --input "data" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_1.1/soft_c012s" --num_classes "51" \
+--training_method "soft_c012s" --model_location "variant_1.1/51_classes" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_all_events" --input "data" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_1.1/regr_c012s" --num_classes "51" \
+--training_method "regr_c012s" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_all_events" --input "data" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_1.1/soft_argmaxs" --num_classes "51" \
+--training_method "soft_argmaxs" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+python main.py --action "test_model_on_all_events" --input "data" \
+--output "plots/figures/test_model_on_all_events/51_classes_variant_1.1/regr_argmaxs" --num_classes "51" \
+--training_method "regr_argmaxs" --model_location "51_classes_variant_1.1" \
+--features "Variant-1.1"
+
+deactivate
